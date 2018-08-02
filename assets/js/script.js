13cb8a762088af5198f78eed0c1762489a82e8a9
John Karaplis <jzkarap@gmail.com>
2018-08-02 16:19:38 -0400

    added local storage to keep persistent to-dos, updated css to truncate long elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+let todoList = [];
+
+// If we have some local storage,
+if (typeof (Storage) !== "undefined") {
+    // Our array of todos is parsed from that local storage
+    todoList = JSON.parse(localStorage.getItem("ToDos"));
+}
+
+// List our todos, pulled from local storage
+listTodos();
+
+// If we have an array of to-dos, list each one
+function listTodos() {
+    if (todoList) {
+        todoList.forEach(function (todo) {
+            $("ul").append("<li class=\"truncate\"><span><i class=\"fas fa-trash-alt\"></i></span>" + todo + "</li>");
+        });
+    }
+}
+
+// Show the most recently added to-do
+function updateTodos() {
+    $("ul").prepend("<li class=\"truncate\"><span><i class=\"fas fa-trash-alt\"></i></span>" + todoList[0] + "</li>");
+}
+
 // Check off to-dos by clicking
 // We can only add a jQuery listener to elements that exist when code is run the first time (so we cannot target li directly --
 // it would not apply this listener to newly created li elements).
@@ -6,17 +31,29 @@ $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-// Click on X to delete to-do
+// Click on trash icon to delete to-do
 $("ul").on("click", "span", function (event) {
-    // "x" is passed in as "event"
+    // "span" is passed in as "event"
     // jQuery method stopPropagation() prevents event bubbling,
     // so only the span containing x is triggered, and the event does not bubble up to parent elements
     event.stopPropagation();
     // Fade out the parent li,
     // then remove it from the DOM
     $(this).parent().fadeOut(500, function () {
+
         // "this" below refers to the li, not the span -- it is "this" parent element passed from above!
+
+        // Get the index of a to-do array element which shares content with the to-do item which has been selected
+        let index = todoList.indexOf(this.innerText);
+        if (index > -1) {
+            // Cut selected to-do from our array
+            todoList.splice(index, 1);
+        }
+        // Remove the selected list item
         $(this).remove();
+
+        // Update our local storage with the new list of to-dos
+        localStorage.setItem("ToDos", JSON.stringify(todoList));
     });
 })
 
@@ -29,11 +66,29 @@ $("input[type='text']").keypress(function (event) {
         // Clear text input by using val() as setter
         $(this).val("");
         // Create new to-do
+        if (typeof (Storage) !== "undefined") {
+            // Store
+            if (todoList) {
+                // Add new to-do to beginning of our to-do list
+                todoList.unshift(newTodo);
+            }
+            else {
+                todoList = [newTodo];
+            }
+            localStorage.setItem("ToDos", JSON.stringify(todoList));
+            // Retrieve
+            // newTodo = JSON.parse((localStorage.getItem("ToDos")));
+            updateTodos();
+        } else {
+            $(this).innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
         // Appends given string to whatever element is selected
-        $("ul").append("<li><span><i class=\"fas fa-trash-alt\"></i></span> " + newTodo + "</li>");
+        // $("ul").append("<li class=\"truncate\"><span><i class=\"fas fa-trash-alt\"></i></span> " + newTodo + "</li>");
     }
 })
 
+// Slides our text input field in and out
+// Updates the icon at top right
 $("h1").on("click", ".far", function () {
     if ($("h1").html() === "To-do List <i class=\"far fa-minus-square\"></i>") {
         $(this).replaceWith("<i class=\"far fa-plus-square\"></i>");
@@ -45,3 +100,4 @@ $("h1").on("click", ".far", function () {
         $("input[type='text']").slideToggle(300);
     }
 })
+
